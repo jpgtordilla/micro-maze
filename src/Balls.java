26a89512b7910ab985aa505dfb78e236fad59e53
Jonathon Paul Guillermo Tordilla <156870738+jpgtordilla@users.noreply.github.com>
2024-02-25 01:15:00 -0800

    successful jpanel implementation—no more flickering

--- src/Balls.java
+++ src/Balls.java
@@ -4,23 +4,45 @@ import java.awt.event.ActionListener;
 import java.util.Random;
 
 import javax.swing.*;
-public class Balls extends MazeGUI implements ActionListener {
-    Timer timer;
-    int x = 100;
-    int y = 100;
+public class Balls extends MazeGUI {
 
     public Balls () {
-        this.setPreferredSize(new Dimension(MazeGUI.SCREENWIDTH, MazeGUI.SCREENHEIGHT));
-        timer = new Timer(10, this);
-        timer.start();
+        super();
+        super.add(new BallPanel());
     }
+    public static class BallPanel extends JPanel implements ActionListener {
+        Timer timer;
+        int x = 100;
+        int xVel = 10;
+        int y = 100;
+        int yVel = 10;
 
-    @Override
-    public void paint(Graphics g) {
-        // generate random circles, then generate one random green circle
-        // player has 2 seconds to click the green circle
-        Graphics2D g2 = (Graphics2D) g;
-        g2.setColor(Color.RED);
-        g2.fillOval(x, y, 50, 50);
+        public BallPanel () {
+            this.setPreferredSize(new Dimension(MazeGUI.SCREENWIDTH, MazeGUI.SCREENHEIGHT));
+            timer = new Timer(10, this);
+            timer.start();
+        }
+
+        @Override
+        public void paint(Graphics g) {
+            // generate random circles, then generate one random green circle
+            // player has 2 seconds to click the green circle
+            Graphics2D g2 = (Graphics2D) g;
+            g2.setColor(Color.RED);
+            g2.fillOval(x, y, 50, 50);
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (x >= MazeGUI.SCREENWIDTH - 50 || x < 0) {
+                xVel = -xVel;
+            }
+            if (y >= MazeGUI.SCREENWIDTH - 50 || y < 0) {
+                yVel = -yVel;
+            }
+            x = x + xVel;
+            y = y + yVel;
+            repaint();
+        }
     }
 }
--- src/MazeGUI.java
+++ src/MazeGUI.java
@@ -5,7 +5,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class MazeGUI extends JFrame implements ActionListener, KeyListener {
+public class MazeGUI extends JFrame {
     public static int playerX = 0;
     public static int playerY = 29;
     public static final int OBJECTDIM = 40;
@@ -14,89 +14,99 @@ public class MazeGUI extends JFrame implements ActionListener, KeyListener {
     // constants
     public static final int SCREENWIDTH = 800;
     public static final int SCREENHEIGHT = 829;
+    MazeGUIPanel panel;
 
     public MazeGUI() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(SCREENWIDTH, SCREENHEIGHT);
-        this.addKeyListener(this);
+        panel = new MazeGUIPanel();
+        this.add(panel);
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.addKeyListener(panel);
         this.setVisible(true);
     }
-    /** returns the direction in a String given a keycode */
-    private String getDirection(int code) {
-        return switch (code) {
-            case 68, 39 -> "RIGHT";
-            case 37, 65 -> "LEFT";
-            case 87, 38 -> "UP";
-            case 83, 40 -> "DOWN";
-            default -> null;
-        };
-    }
-    /** take in details from Maze class and render to the Swing Panel */
-    public void paint(Graphics g) {
-        super.paint(g);
-        Graphics2D g2 = (Graphics2D) g;
-        if (Maze.runningMaze) {
+
+    public static class MazeGUIPanel extends JPanel implements KeyListener, ActionListener {
+        public MazeGUIPanel() {
+            this.setPreferredSize(new Dimension(SCREENWIDTH, SCREENHEIGHT));
+            this.setBackground(Color.GREEN);
+        }
+        /** take in details from Maze class and render to the Swing Panel */
+        public void paint(Graphics g) {
+            super.paint(g);
+            Graphics2D g2 = (Graphics2D) g;
             renderArray(g2);
         }
-    }
-    public void renderArray(Graphics2D g2) {
-        // render player
-        g2.setColor(Color.RED);
-        g2.fillRect(playerX, playerY, OBJECTDIM, OBJECTDIM);
-        // render goal
-        g2.setColor(Color.ORANGE);
-        g2.fillRect(goalX, goalY, OBJECTDIM, OBJECTDIM);
-        // render maze
-        g2.setColor(Color.BLACK);
-        int xPos = 0;
-        int yPos = 29;
-        for (int i = 0; i < Maze.MAZEDIM; i++) {
-            for (int j = 0; j < Maze.MAZEDIM; j++) {
-                // screen dims:
-                // * 0-800
-                // * 29-839
-                if (Maze.mazeArr[i][j].equals("X")) {
-                    g2.fillRect(xPos, yPos, OBJECTDIM, OBJECTDIM);
+        public void renderArray(Graphics2D g2) {
+            // render player
+            g2.setColor(Color.RED);
+            g2.fillRect(playerX, playerY, OBJECTDIM, OBJECTDIM);
+            // render goal
+            g2.setColor(Color.ORANGE);
+            g2.fillRect(goalX, goalY, OBJECTDIM, OBJECTDIM);
+            // render maze
+            g2.setColor(Color.BLACK);
+            int xPos = 0;
+            int yPos = 29;
+            for (int i = 0; i < Maze.MAZEDIM; i++) {
+                for (int j = 0; j < Maze.MAZEDIM; j++) {
+                    // screen dims:
+                    // * 0-800
+                    // * 29-839
+                    if (Maze.mazeArr[i][j].equals("X")) {
+                        g2.fillRect(xPos, yPos, OBJECTDIM, OBJECTDIM);
+                    }
+                    xPos += 40;
                 }
-                xPos += 40;
-            }
-            xPos = 0;
-            yPos += 40;
-        }
-    }
-    @Override
-    public void keyTyped(KeyEvent e) {}
-    @Override
-    public void keyPressed(KeyEvent e) {}
-    @Override
-    public void keyReleased(KeyEvent e) {
-        // System.out.println("Key pressed: " + getDirection(e.getKeyCode()));
-        if (Maze.runningMaze) {
-            // update player position based on input
-            switch (getDirection(e.getKeyCode())) {
-                case "RIGHT":
-                    Maze.handleMovement("RIGHT");
-                    repaint();
-                    break;
-                case "LEFT":
-                    Maze.handleMovement("LEFT");
-                    repaint();
-                    break;
-                case "UP":
-                    Maze.handleMovement("UP");
-                    repaint();
-                    break;
-                case "DOWN":
-                    Maze.handleMovement("DOWN");
-                    repaint();
-                    break;
-                default:
-                    break;
+                xPos = 0;
+                yPos += 40;
             }
         }
-    }
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        repaint();
+        /** returns the direction in a String given a keycode */
+        private String getDirection(int code) {
+            return switch (code) {
+                case 68, 39 -> "RIGHT";
+                case 37, 65 -> "LEFT";
+                case 87, 38 -> "UP";
+                case 83, 40 -> "DOWN";
+                default -> null;
+            };
+        }
+        @Override
+        public void keyTyped(KeyEvent e) {}
+        @Override
+        public void keyPressed(KeyEvent e) {}
+        @Override
+        public void keyReleased(KeyEvent e) {
+            // System.out.println("Key pressed: " + getDirection(e.getKeyCode()));
+            if (Maze.runningMaze) {
+                // update player position based on input
+                switch (getDirection(e.getKeyCode())) {
+                    case "RIGHT":
+                        Maze.handleMovement("RIGHT");
+                        repaint();
+                        break;
+                    case "LEFT":
+                        Maze.handleMovement("LEFT");
+                        repaint();
+                        break;
+                    case "UP":
+                        Maze.handleMovement("UP");
+                        repaint();
+                        break;
+                    case "DOWN":
+                        Maze.handleMovement("DOWN");
+                        repaint();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            repaint();
+        }
     }
 }
